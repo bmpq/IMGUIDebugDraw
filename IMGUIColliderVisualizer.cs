86e8ae5d4ad969a8b8dc7dcce1f6040e2b2cdb75
bmpq <73074311+bmpq@users.noreply.github.com>
2025-04-15 03:20:31 +0300

    Create IMGUIColliderVisualizer.cs

---
+++ IMGUIColliderVisualizer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace IMGUIDebugDraw
+{
+    public class IMGUIColliderVisualizer : MonoBehaviour
+    {
+        Camera cam;
+        Collider col;
+
+        void Start()
+        {
+            cam = Camera.main;
+            col = GetComponent<Collider>();
+        }
+
+        void OnGUI()
+        {
+            if (col is BoxCollider boxCol)
+            {
+                Draw.BoxCollider(cam, boxCol, Color.green);
+            }
+        }
+    }
+}
